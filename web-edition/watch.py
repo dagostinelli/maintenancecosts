061aa21d8c9d57ccdf3de8f51e337863ec46c5bd
Darryl T. Agostinelli <dagostinelli@gmail.com>
2018-09-15 15:04:17 -0500

    adjusted watch

--- web-edition/watch.py
+++ web-edition/watch.py
@@ -9,9 +9,10 @@ notifier = pyinotify.Notifier(wm)
 mask = pyinotify.IN_CLOSE_WRITE
 
 wm.add_watch('../book/', mask)
+wm.add_watch('templates/', mask)
 
 def on_event(notifier, stuff):
-	call(["make", "build"])
+	call(["./build.sh"])
 
 on_loop_func = functools.partial(on_event, stuff=dict())
 
